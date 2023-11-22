73f5eb6a136205ca645fd27212fb65e24de83351
smith_fernandes_dell <mscf@icomp.ufam.edu.br>
2023-11-21 20:27:49 -0400

    GridView com Expanded

--- lib/pages/ArCondicionadoPage.dart
+++ lib/pages/ArCondicionadoPage.dart
@@ -22,38 +22,52 @@ class _ArConficionadoPage extends State<ArConficionadoPage>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start, //Center Row contents horizontally,
-          crossAxisAlignment: CrossAxisAlignment.center, //Center Row contents vertically,
-          children: [
-            SizedBox(height: alturaEntreLinhas),
-            Container(
-              width: 200,
-              decoration: BoxDecoration(
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.5),
-                    spreadRadius: 5,
-                    blurRadius: 7,
-                    offset: Offset(0, 3),
+      body: Column(
+        children: [
+          SizedBox(height: alturaEntreLinhas),
+          Container(
+            width: 200,
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.5),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: Offset(0, 3),
+                ),
+              ],
+            ),
+            child: Image.asset("assets/images/arcondicionado.jpg", width: 200),
+          ),
+          SizedBox(height: alturaEntreLinhas),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(20, 5, 20, 20),
+              child: GridView.count(
+                crossAxisCount: 2,
+                mainAxisSpacing: 15,
+                crossAxisSpacing: 15,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), color: Colors.red,),
+                    height: 10,
+                    width: 10,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), color: Colors.blue,),
+                    height: 10,
+                    width: 10,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), color: Colors.green,),
+                    height: 10,
+                    width: 10,
                   ),
                 ],
               ),
-              child: Image.asset("assets/images/arcondicionado.jpg", width: 200),
             ),
-            SizedBox(height: alturaEntreLinhas),
-            Container(
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: GridView(
-                  children: [Container(decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), color: Colors.red,),),],
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 1, mainAxisSpacing: 10, crossAxisSpacing: 10,),
-                ),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
