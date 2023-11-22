a481eee79ffd324d9f6ed042c79d204463e2a312
smith_fernandes_dell <mscf@icomp.ufam.edu.br>
2023-11-21 22:14:39 -0400

    Ink e InkWell

--- lib/pages/ArCondicionadoPage.dart
+++ lib/pages/ArCondicionadoPage.dart
@@ -21,53 +21,108 @@ class _ArConficionadoPage extends State<ArConficionadoPage>{
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-      body: Column(
-        children: [
-          SizedBox(height: alturaEntreLinhas),
-          Container(
-            width: 200,
-            decoration: BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.5),
-                  spreadRadius: 5,
-                  blurRadius: 7,
-                  offset: Offset(0, 3),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(height: alturaEntreLinhas),
+              Container(
+                width: 200,
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-            child: Image.asset("assets/images/arcondicionado.jpg", width: 200),
-          ),
-          SizedBox(height: alturaEntreLinhas),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(20, 5, 20, 20),
-              child: GridView.count(
-                crossAxisCount: 2,
-                mainAxisSpacing: 15,
-                crossAxisSpacing: 15,
+                child: Image.asset("assets/images/arcondicionado.jpg", width: 200),
+              ),
+              SizedBox(height: alturaEntreLinhas),
+
+              //linha 1
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Container(
-                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), color: Colors.red,),
-                    height: 10,
-                    width: 10,
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Container(
+                      decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), color: Colors.red,),
+                      height: 200,
+                      width: 200,
+                    ),
                   ),
-                  Container(
-                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), color: Colors.blue,),
-                    height: 10,
-                    width: 10,
-                  ),
-                  Container(
-                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), color: Colors.green,),
-                    height: 10,
-                    width: 10,
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), color: Colors.blue,),
+                      height: 200,
+                      width: 200,
+                    ),
                   ),
                 ],
               ),
-            ),
+
+              //linha 2
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: InkWell(
+                      onTap: () {
+                        setState(() {
+                          Colors.white;
+                        });
+                      },
+                      onLongPress: () {
+                        setState(() {
+                          Colors.green;
+                        });
+                      },
+                      child: Ink(
+                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), color: Color(0xFF142434),),
+                        height: 200,
+                        width: 200,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              child: Image.asset("assets/icons/airconditioner.png", width: 100, color: Colors.white,),
+                            ),
+                            Text("Ar Condicionado 3", style: TextStyle(fontSize: 16, color: Colors.white),)
+                          ],
+                        ),
+                      ),
+                    ),
+                    // child: Ink(
+                    //   decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), color: Color(0xFF142434),),
+                    //   height: 200,
+                    //   width: 200,
+                    //   child: InkWell(
+                    //     child: Column(
+                    //       mainAxisAlignment: MainAxisAlignment.center,
+                    //       children: [
+                    //         Container(
+                    //           child: Image.asset("assets/icons/airconditioner.png", width: 100, color: Colors.white,),
+                    //         ),
+                    //         Text("Ar Condicionado 3", style: TextStyle(fontSize: 16, color: Colors.white),)
+                    //       ],
+                    //     ),
+                    //   )
+                    // ),
+                  ),
+                ],
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
